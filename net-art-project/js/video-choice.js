f23abd274655b731949cce6e7b6e0a978a16d796
hereisben <jl.jimluv@gmail.com>
2025-03-08 16:04:15 +0700

    update net-art-project
    update full video source

--- net-art-project/js/video-choice.js
+++ net-art-project/js/video-choice.js
@@ -1,23 +1,7 @@
-// const VIDEO_SOURCE = document.getElementById("video-source");
-// const copaRUNNERUP_BTNS = document.querySelectorAll(".coparunnerup");
-// const copaWINNER_BTNS = document.querySelectorAll(".copawinner");
-
-// copaRUNNERUP_BTNS.forEach((copaRUNNERUP_BTN) => {
-//   copaRUNNERUP_BTN.addEventListener("click", () => {
-//     VIDEO_SOURCE.src =
-//       "https://www.youtube.com/embed/_wmhadwpL7I?si=rHF5NIBsNJCf_CvP";
-//   });
-// });
-
-// copaWINNER_BTNS.forEach((copaWINNER_BTN) => {
-//   copaWINNER_BTN.addEventListener("click", () => {
-//     VIDEO_SOURCE.src =
-//       "https://www.youtube.com/embed/XR1ygMVRg9Y?si=cJr9tbqpyEA3BlVq";
-//   });
-// });
-
 const VIDEO_SOURCE = document.getElementById("video-source");
-const videoButtons = document.querySelectorAll(".coparunnerup, .copawinner");
+const videoButtons = document.querySelectorAll(
+  ".coparunnerup, .copawinner, .worldcupwinner, .psgtransfer, .firstballondor, .newell, .lamasia, .sextuple, .worldcuprunnerup, .firstgoal, .mlsmove, .olympic, .firstchampionleague, .barcelonadebut"
+);
 
 // Hide the iframe initially
 VIDEO_SOURCE.style.display = "none";
@@ -26,6 +10,23 @@ VIDEO_SOURCE.style.display = "none";
 const videoSources = {
   coparunnerup: "https://www.youtube.com/embed/_wmhadwpL7I?si=rHF5NIBsNJCf_CvP",
   copawinner: "https://www.youtube.com/embed/XR1ygMVRg9Y?si=cJr9tbqpyEA3BlVq",
+  worldcupwinner:
+    "https://www.youtube.com/embed/ZmKy_fnRM_E?si=uydRPLN2vfzVECB8",
+  psgtransfer: "https://www.youtube.com/embed/dTKl0pcC1a4?si=Xvk_v5D3BlsPwPhq",
+  firstballondor:
+    "https://www.youtube.com/embed/ghHyRqlbgCU?si=WT8WGMGmy6d4vkZv",
+  newell: "https://www.youtube.com/embed/0j9POXpurPU?si=J8OAbbAenuV0ktGQ",
+  lamasia: "https://www.youtube.com/embed/RaQvhgTu7QY?si=hK27GGK1n54LLP_B",
+  sextuple: "https://www.youtube.com/embed/LdmVkqxqvrk?si=vXlGZbbKwM7qEHll",
+  worldcuprunnerup:
+    "https://www.youtube.com/embed/DFeu4QMYdwo?si=3fosvEXaTfcsP72l",
+  firstgoal: "https://www.youtube.com/embed/j9ihJMqZGD0?si=MU_8MAV6OqvODekZ",
+  mlsmove: "https://www.youtube.com/embed/esB0AkFIDeE?si=X5vgc1YySbaOEHk8",
+  olympic: "https://www.youtube.com/embed/Dqevl3w0_6s?si=idJe55bWJGWFg94E",
+  firstchampionleague:
+    "https://www.youtube.com/embed/Uf-6aRJk8q0?si=9fzCLhaPboBZDTFh",
+  barcelonadebut:
+    "https://www.youtube.com/embed/5iWf1yczpTg?si=-oEme8b_lyLHyDUp",
 };
 
 // Add a single event listener to handle all video buttons

296afb287060bfa2916bb5e04cbafb7fd5446a61
hereisben <jl.jimluv@gmail.com>
2025-04-03 13:35:53 +0700

    update date personal portfolio
    Fix cursor effect by using esm.sh to resolve threejs-toys module dependencies

--- new-portfolio/assets/js/cursor-effect.js
+++ new-portfolio/assets/js/cursor-effect.js
@@ -1,4 +1,4 @@
-import { neonCursor } from "https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js";
+import { neonCursor } from "https://esm.sh/threejs-toys@0.0.8";
 
 neonCursor({
   el: document.getElementById("blackout"),
